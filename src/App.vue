8b9cbd31966c961322fc61e892ccf12de3891203
Renan Santana <renan_ota@hotmail.com>
2022-05-30 00:20:39 -0300

    base guestbook app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GuestBook/>
 </template>
 
+
+<style>
+body {
+  background-color: blueviolet;
+  min-height: 100vh;
+}
+</style>
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GuestBook from './views/GuestBook.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GuestBook
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/GuestBook.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+
+    <div class="content">
+      <h1>GuestBook</h1>
+      <h5>Deixe sua mensagem!</h5>
+    </div>
+
+    <div v-for="message in messages" :key="message.id" class="box">
+      <div class="content">
+        <h3>{{ message.author }}</h3>
+        <p>{{ message.text }}</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  color: whitesmoke;
+}
+h5 {
+  color: whitesmoke;
+}
+</style>
+
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'GuestBook',
+
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          author: "Renan",
+          text: "teste teste teste"
+        },
+        {
+          id: 2,
+          author: "Samuel",
+          text: "vo te pega no soco!"
+        },
+      ]
+    }
+  },
+
+  created() {
+    const api_url = `${process.env.VUE_APP_API_URL}/messages`
+    axios.get(api_url).then((response) => {
+      console.log(response);
+      this.messages = response.data
+    })
+  },
+}
+</script>
